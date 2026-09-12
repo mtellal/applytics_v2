54aca66405aa718fcd027b2a0ad7193d0fe6b5c5
mtellal <mez.tellal@gmail.com>
2026-09-12 17:09:42 +0200

    feat: supabase client added with google oauth

---
+++ src/features/auth/services/auth.service.ts
@@ -0,0 +1,19 @@
+import { supabase } from '@/lib/supabase';
+
+export async function singInWithGoogle() {
+  const { data, error } = await supabase.auth.signInWithOAuth({
+    provider: 'google',
+    options: {
+      redirectTo: `${window.location.origin}/dashboard`,
+    },
+  });
+
+  if (error) throw error;
+
+  return data;
+}
+
+export async function signOut() {
+  const { error } = await supabase.auth.signOut();
+  if (error) throw error;
+}
