96019999b8efcb5acbb120e16920ba966ef347de
mtellal <mez.tellal@gmail.com>
2026-09-15 15:13:47 +0200

    fix: resolved dashboard conflicts

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -27,17 +27,10 @@ export default function Dashboard() {
     loadingRecents,
   } = useDashboard();
 
-<<<<<<< Updated upstream:src/pages/Dashboard.tsx
   const {} = useAuth();
 
-=======
-<<<<<<< Updated upstream:src/pages/Dahsboard.tsx
-=======
-  console.log(applicationsActivity);
-
   const { user } = useAuth();
->>>>>>> Stashed changes:src/pages/Dashboard.tsx
->>>>>>> Stashed changes:src/pages/Dahsboard.tsx
+
   return (
     <main className="min-h-screen space-y-3 p-4">
       <DashboardHeader name={user?.identities?.[0].identity_data?.name.split(' ')[0]} />
